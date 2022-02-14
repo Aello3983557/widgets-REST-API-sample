a724bff0ebea9317ff4fdda09805b7f7a881c46d
19644374 <KAShpakov@sberbank.ru>
2022-02-14 03:00:58 +0300

    WIDGETS-REST-API controller refactoring

--- src/main/java/com/aello/web/controller/WidgetController.java
+++ src/main/java/com/aello/web/controller/WidgetController.java
@@ -11,11 +11,10 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 import java.util.List;
-import java.util.UUID;
 
 import static com.aello.constants.ControllerDocumentationConstants.*;
 
-@Api(tags = API_TAGS)
+@Api(tags = API_TAGS, value = "1")
 @RestController
 @RequestMapping(value = WIDGET_MAPPING)
 public class WidgetController {
@@ -26,39 +25,41 @@ public class WidgetController {
         this.widgetService = widgetService;
     }
 
-    @ApiOperation(value = CREATE_WIDGET_DESCRIPTION, notes = CREATE_WIDGET_NOTES)
+    @ApiOperation(value = CREATE_WIDGET_DESCRIPTION, notes = CREATE_WIDGET_NOTES, response = Widget.class)
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = WIDGET_CREATED_SUCCESSFULLY),
             @ApiResponse(code = 400, message = WIDGET_ID_IS_NOT_EMPTY_EXCEPTION_MESSAGE)})
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Widget> createWidget(
             @ApiParam(name = WIDGET, value = WIDGET_TO_CREATE)
-            @Valid
-            @RequestBody Widget widget) {
+            @RequestBody @Valid Widget widget) {
         return new ResponseEntity<>(widgetService.createWidget(widget), HttpStatus.CREATED);
     }
 
-    @ApiOperation(value = UPDATE_WIDGET_DESCRIPTION, notes = UPDATE_WIDGET_NOTES)
+    @ApiOperation(value = UPDATE_WIDGET_DESCRIPTION, notes = UPDATE_WIDGET_NOTES, response = Widget.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = WIDGET_UPDATED_SUCCESSFULLY),
             @ApiResponse(code = 400, message = WIDGET_ID_IS_EMPTY_EXCEPTION_MESSAGE),
             @ApiResponse(code = 404, message = WIDGET_NOT_FOUND_EXCEPTION_MESSAGE)})
-    @PutMapping()
+    @PatchMapping(WIDGET_UUID_MAPPING)
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Widget> updateWidget(
             @ApiParam(name = WIDGET, value = WIDGET_TO_UPDATE)
-            @Valid
-            @RequestBody Widget widget) {
-        return new ResponseEntity<>(widgetService.updateWidget(widget), HttpStatus.OK);
+            @PathVariable(WIDGET_UUID) String widgetUUID,
+            @RequestBody @Valid Widget widget) {
+        return new ResponseEntity<>(widgetService.updateWidget(widgetUUID, widget), HttpStatus.OK);
     }
 
     @ApiOperation(value = DELETE_WIDGET_DESCRIPTION)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = WIDGET_DELETED_SUCCESSFULLY),
             @ApiResponse(code = 404, message = WIDGET_NOT_FOUND_EXCEPTION_MESSAGE)})
-    @DeleteMapping()
+    @DeleteMapping(WIDGET_UUID_MAPPING)
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<HttpStatus> deleteWidget(
             @ApiParam(name = WIDGET_UUID, value = WIDGET_UUID_DESCRIPTION)
-            @RequestParam() UUID widgetUUID) {
+            @PathVariable(WIDGET_UUID) String widgetUUID) {
         widgetService.deleteWidgetByUUID(widgetUUID);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -67,10 +68,11 @@ public class WidgetController {
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = WIDGET_FOUND),
             @ApiResponse(code = 404, message = WIDGET_NOT_FOUND_EXCEPTION_MESSAGE)})
-    @GetMapping()
+    @GetMapping(WIDGET_UUID_MAPPING)
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Widget> getWidget(
             @ApiParam(name = WIDGET_UUID, value = WIDGET_UUID_DESCRIPTION)
-            @RequestParam UUID widgetUUID) {
+            @PathVariable(WIDGET_UUID) String widgetUUID) {
         return new ResponseEntity<>(widgetService.getWidgetByUUID(widgetUUID), HttpStatus.OK);
     }
 
@@ -78,7 +80,8 @@ public class WidgetController {
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = WIDGET_LIST_PROVIDED),
             @ApiResponse(code = 404, message = EMPTY_WIDGET_STORAGE_EXCEPTION_MESSAGE)})
-    @GetMapping(value = ALL_WIDGET_MAPPING)
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<Widget>> getWidgetsList() {
         return new ResponseEntity<>(widgetService.getAllWidgets(), HttpStatus.OK);
     }
